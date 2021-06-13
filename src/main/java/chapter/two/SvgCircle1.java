aa1f49ce5da4a481ece3218c89d7fea3c2def0b3
kellerSeb <se.kellermann@gmail.com>
2021-06-13 14:12:27 +0200

    svg class. Exercice 2.1.2

---
+++ src/main/java/chapter/two/SvgCircle1.java
@@ -0,0 +1,11 @@
+package chapter.two;
+
+//https://www.w3schools.com/graphics/tryit.asp?filename=trysvg_circle
+
+public class SvgCircle1 {
+    public static void main(String[] args) {
+        System.out.println("<svg height =\"400\" width=\"1000\">");
+        System.out.println("<circle cx=\"100\" cy=\"100\" r=\"50\" />");
+        System.out.println("</svg>");
+    }
+}
