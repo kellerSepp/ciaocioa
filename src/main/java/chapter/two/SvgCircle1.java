abb3ba4b63a3d4077906d230a7e24f3d78dbf9a3
kellerSeb <se.kellermann@gmail.com>
2021-06-13 16:56:09 +0200

    2.2.4 2.2.5

--- src/main/java/chapter/two/SvgCircle1.java
+++ src/main/java/chapter/two/SvgCircle1.java
@@ -2,21 +2,48 @@ package chapter.two;
 
 //https://www.w3schools.com/graphics/tryit.asp?filename=trysvg_circle
 
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class SvgCircle1 {
 
     public static void main(String[] args) {
         int x = 100;
         int y = 100;
-        double r = 20.5;
+        double r = ThreadLocalRandom.current().nextDouble(10,20);
         System.out.println("<svg height =\"400\" width=\"1000\">");
         System.out.println("<circle cx=\"" + x + "\" cy=\"" + y + "\" r=\"" + r + "\" />");
         System.out.println("</svg>");
 
+        System.out.println("\n######################################\n");
+
         System.out.println("Wertebereich bei int 1000000 * 1000000 = " + 1000000 * 1000000);
+
+        System.out.println("\n######################################\n");
 
         System.out.println("Genauigkeit bei:");
         System.out.print("0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 - 0.1 = ");
         System.out.println(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 - 0.1);
-        
+
+        System.out.println("\n######################################\n");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("cx (ganze Zahl bitte) = ");
+        int cx = scanner.nextInt();
+        System.out.println("cy (ganze Zahl bitte) = ");
+        int cy = scanner.nextInt();
+        System.out.println("<svg height =\"400\" width=\"1000\">");
+        System.out.println("<circle cx=\"" + cx + "\" cy=\"" + cy + "\" r=\"" + r + "\" />");
+        System.out.println("</svg>");
+
+        System.out.println("\n######################################\n");
+
+
+        System.out.println("\n######################################\n");
+
+
+
+
+
     }
 }
