b5dc4dd469d09d3660fd6bb7fc454a1d9f0c9fff
kellerSeb <se.kellermann@gmail.com>
2021-06-13 13:19:35 +0200

    - new project. - first excercises - first JUnit5 Test for Calculator

---
+++ src/main/java/chapter/one/ApplicationOne.java
@@ -0,0 +1,7 @@
+package chapter.one;
+
+public class ApplicationOne {
+    public static void main(String[] args) {
+        System.out.println("Aye Captain!");
+    }
+}
---
+++ src/main/java/chapter/one/ApplicationTwo.java
@@ -0,0 +1,12 @@
+package chapter.one;
+
+
+/***
+ * according to the book there should be a bunch of mistakes and errors in that class.
+ * The want first users to understand more about syntax, etc.
+ */
+public class ApplicationTwo {
+    public static void main(String[] args) {
+        System.out.println("Aye Captain!");
+    }
+}
