14a24dd9f49af8826ab776920c6a9decf038320d
kellerSeb <se.kellermann@gmail.com>
2021-06-13 14:36:33 +0200

    2.2.1 2.2.2 2.2.3

--- src/main/java/chapter/two/SvgCircle1.java
+++ src/main/java/chapter/two/SvgCircle1.java
@@ -3,9 +3,20 @@ package chapter.two;
 //https://www.w3schools.com/graphics/tryit.asp?filename=trysvg_circle
 
 public class SvgCircle1 {
+
     public static void main(String[] args) {
+        int x = 100;
+        int y = 100;
+        double r = 20.5;
         System.out.println("<svg height =\"400\" width=\"1000\">");
-        System.out.println("<circle cx=\"100\" cy=\"100\" r=\"50\" />");
+        System.out.println("<circle cx=\"" + x + "\" cy=\"" + y + "\" r=\"" + r + "\" />");
         System.out.println("</svg>");
+
+        System.out.println("Wertebereich bei int 1000000 * 1000000 = " + 1000000 * 1000000);
+
+        System.out.println("Genauigkeit bei:");
+        System.out.print("0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 - 0.1 = ");
+        System.out.println(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 - 0.1);
+        
     }
 }
